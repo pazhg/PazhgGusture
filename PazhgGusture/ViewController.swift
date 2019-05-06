3d313c8a4656dcc3b6ed95ab6ac248cd152a249b
Kambiz Guity <kg@pazhg.com>
2019-05-06 13:07:29 +0430

    Initial Commit

---
+++ PazhgGusture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PazhgGusture
+//
+//  Created by Pazhg on 2/16/1398 AP.
+//  Copyright © 1398 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
