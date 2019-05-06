c87277452560eab0925e112d966855c76ba27034
Kambiz Guity <kg@pazhg.com>
2019-05-06 19:41:23 +0430

    Design UI & Views

---
+++ PazhgGusture/DiscreteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiscreteViewController.swift
+//  PazhgGusture
+//
+//  Created by Pazhg on 2/16/1398 AP.
+//  Copyright © 1398 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class DiscreteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PazhgGusture/ViewController.swift
+++ PazhgGusture/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonDiscrete: UIButton!
+    
+    @IBOutlet weak var buttonContinues: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        buttonDiscrete.layer.cornerRadius = 10
+        buttonDiscrete.layer.borderWidth = 1
+        buttonDiscrete.layer.borderColor = UIColor.green.cgColor
+        
+        buttonContinues.layer.cornerRadius = 10
+        buttonContinues.layer.borderWidth = 1
+        buttonContinues.layer.borderColor = UIColor.blue.cgColor
+ 
+    }
+    
+    @IBAction func closeView ( segue : UIStoryboardSegue ) {
+        
     }
 
 
